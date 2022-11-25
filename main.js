ec1ee5ef7eacd5b4754ff45747dea99f69ab2247
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2022-11-25 11:19:07 -0500

    Add new task to localStorage

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 window.addEventListener('load', () => {
     tasks = JSON.parse(localStorage.getItem('tasks')) || [];
     const nameInput = document.querySelector('#name');
-    const newTaskForm = document.querySelector('#new-task-form');
+    const newTaskForm = document.querySelector('#new-form');
 
     //set the user's name at the top onChange
     const username = localStorage.getItem('username') || '';
@@ -10,5 +10,20 @@ window.addEventListener('load', () => {
     nameInput.addEventListener('change', e => {
         localStorage.setItem('username', e.target.value);
     });
+
+    newTaskForm.addEventListener('submit', e => {
+        e.preventDefault();
+
+        const task = {
+            content: e.target.elements.content.value,
+            category: e.target.elements.content.value,
+            done: false,
+            createdAt: new Date().getTime()
+        }
+
+        tasks.push(task);
+ 
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    })
 });
 
