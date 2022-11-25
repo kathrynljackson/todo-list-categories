d6c6a82f7cb69c90bf7a493df7a28913ef939774
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2022-11-25 12:00:04 -0500

    Render tasks

--- main.js
+++ main.js
@@ -29,6 +29,60 @@ window.addEventListener('load', () => {
 
         // reset form input
         e.target.reset();
+
+        // Display tasks from local storage
+        DisplayTasks();
     })
-});
+
+    DisplayTasks();
+})
+
+function DisplayTasks() {
+    console.log('DISPLAY TASKS RUNNING')
+    const todoList = document.querySelector('#todo-list');
+    todoList.innerHTML = '';
+
+    // Iterate through the array of tasks and create an individual element for each one
+    tasks.forEach(taskItem => {
+        const task = document.createElement('div');
+        task.classList.add('todo-item');
+        const label = document.createElement('label');
+        const input = document.createElement('input');
+        const span = document.createElement('span');
+        const content = document.createElement('div');
+        const actions = document.createElement('div');
+        const edit = document.createElement('button');
+        const deleteTaskBtn = document.createElement('button');
+
+        input.type = 'checkbox';
+        input.checked = taskItem.done;
+        span.classList.add('bubble');
+
+        if(taskItem.category == 'home') {
+            span.classList.add('home');
+        } else if(taskItem.category == 'work') {
+            span.classList.add('work');
+        }
+
+        content.classList.add('todo-content');
+        actions.classList.add('actions');
+        edit.classList.add('edit');
+        deleteTaskBtn.classList.add('delete');
+
+        content.innerHTML = `<input type="text" value="${taskItem.content}" readonly>`
+        edit.innerHTML = 'Edit';
+        deleteTaskBtn.innerHTML = 'Delete';
+
+        label.appendChild(input);
+        label.appendChild(span);
+        actions.appendChild(edit);
+        actions.appendChild(deleteTaskBtn);
+        task.appendChild(label);
+        task.appendChild(content);
+        task.appendChild(actions);
+        
+        todoList.appendChild(task);
+    })
+}
+
 
